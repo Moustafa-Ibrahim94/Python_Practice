160af914dce96ae641863e505d7483a7f6e1e4fc
Mustafa Elsawy <31455797+mustafaelsawy56@users.noreply.github.com>
2022-07-25 22:18:08 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,11 +3,13 @@ import Intersect_Function
 
 
 class MyTestCase(unittest.TestCase):
+    # here i'm checking my intersect_distance function output
     def test_myTestCase(self):
         line_1 = {(2, -1), (2, 1)}
         angle = 45
-        result = Intersect_Function.intersect_distance(angle, line_1)
-        self.assertEqual(result, ({(2, -1), (2, 1)}, 2.83))
+        right_output = ({(2, -1), (2, 1)}, 2.83) # the correct output
+        result = Intersect_Function.intersect_distance(angle, line_1) # the function output
+        self.assertEqual(result, right_output ) # checking equality
 
 
 if __name__ == '__main__':
